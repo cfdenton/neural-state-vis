bebf80ac81f6079d910361de3eda0fdf45bd6d86
Carl Denton <carldenton@college.harvard.edu>
2016-08-18 00:34:39 -0400

    basic vis working

--- vis.js
+++ vis.js
@@ -5,23 +5,29 @@ function useData(data) {
       rowArray = Object.keys(row).map(function (k) {return row[k];});
       return rowArray;
    });
+
    console.log(dataset)
-   var paragraphs = d3.select("body")
+   var fieldHeight = 800
+   var fieldWidth = 1200
+   var rows = d3.select("body")
       .append("center")
       .append("svg")
-      .attr("height", 1000)
-      .attr("width", 1000)
+      .attr("height", fieldHeight)
+      .attr("width", fieldWidth)
       .selectAll("g")
       .data(dataset)
       .enter()
-      .append("rect")
-      .attr("x", function (d, i) { return i * 100; }) 
-      .attr("y", function (d, i) { return i * 100; })
-      .attr("width", 100)
-      .attr("height", 100)
-      .style("fill", "blue")
-      .style("opacity", function (d) {
-         console.log(1/(1 + Math.exp(-d[0]))); 
-         return 1/(1 + Math.exp(-d[0])); } );
+      .each(function (d, i, nodes) {
+         console.log("length: " + d.length)
+         for (j in d) {
+            d3.select(this).append("rect") 
+            .attr("x", i * fieldWidth / nodes.length) 
+            .attr("y", j * fieldHeight / d.length)
+            .attr("width", fieldWidth / nodes.length)
+            .attr("height", fieldHeight / d.length)
+            .style("fill", "blue")
+            .style("opacity", 1/(1 + Math.exp(-d[j])));
+         };
+      });
 }
 
