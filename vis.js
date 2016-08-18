bce0bf88e1ef38fe3e5fc5812c32a3badb31bb55
Carl Denton <carldenton@college.harvard.edu>
2016-08-17 20:36:07 -0400

    data input working

--- vis.js
+++ vis.js
@@ -1,16 +1,27 @@
 d3.csv("tensor.csv", useData);
 
 function useData(data) {
-   var paragraphs = d3.select("body").selectAll("p")
-      .data(data)
-      .enter()
-      .append("center")
-      .append("p")
-      .text(function(d) {
-         s = "";
-         for (col in d){
-            s = s + d[col] + " / ";
-         }
-      return s;
+   dataset = data.map(function (row) {
+      rowArray = Object.keys(row).map(function (k) {return row[k];});
+      return rowArray;
    });
+   console.log(dataset)
+   var paragraphs = d3.select("body")
+      .append("center")
+      .append("svg")
+      .attr("height", 1000)
+      .attr("width", 1000)
+      .selectAll("g")
+      .data(dataset)
+      .enter()
+      .append("rect")
+      .attr("x", function (d, i) { return i * 100; }) 
+      .attr("y", function (d, i) { return i * 100; })
+      .attr("width", 100)
+      .attr("height", 100)
+      .style("fill", "blue")
+      .style("opacity", function (d) {
+         console.log(1/(1 + Math.exp(-d[0]))); 
+         return 1/(1 + Math.exp(-d[0])); } );
 }
+
