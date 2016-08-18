c4d308c7c3eca1f978228fc521bf2b532c48bb63
Carl Denton <carldenton@college.harvard.edu>
2016-08-18 12:44:34 -0400

    rounded rect

--- vis.js
+++ vis.js
@@ -7,27 +7,64 @@ function useData(data) {
    });
 
    console.log(dataset)
-   var fieldHeight = 800
-   var fieldWidth = 1200
-   var rows = d3.select("body")
+   var fieldHeight = 400
+   var fieldWidth = 450
+   var svg = d3.select("body")
       .append("center")
       .append("svg")
       .attr("height", fieldHeight)
       .attr("width", fieldWidth)
-      .selectAll("g")
+   
+   var background = svg.append("rect")
+      .attr("height", "100%")
+      .attr("width", "100%")
+      .style("fill", "white")
+      .attr("rx", 10)
+      .attr("ry", 10)
+
+   var rectangles = svg.selectAll("g")
       .data(dataset)
       .enter()
       .each(function (d, i, nodes) {
          console.log("length: " + d.length)
          for (j in d) {
             d3.select(this).append("rect") 
-            .attr("x", i * fieldWidth / nodes.length) 
-            .attr("y", j * fieldHeight / d.length)
-            .attr("width", fieldWidth / nodes.length)
-            .attr("height", fieldHeight / d.length)
-            .style("fill", "blue")
-            .style("opacity", 1/(1 + Math.exp(-d[j])));
+            .attr("x", j * fieldWidth / d.length) 
+            .attr("y", i * fieldHeight / nodes.length)
+            .attr("rx", 5)
+            .attr("ry", 5)
+            .attr("width", fieldWidth / d.length)
+            .attr("height", fieldHeight / nodes.length)
+            .style("fill", function () { 
+               console.log(d[j])
+               if (d[j] >= 0) { return "blue"; }
+               else { return "red"; };
+            })
+            .style("opacity", 1/(1 + Math.exp(-Math.abs(d[j]))))
          };
       });
-}
+   /*
+   var circles = svg.selectAll("g")
+      .data(dataset)
+      .enter()
+      .each(function (d, i, nodes) {
+         console.log("lenth: " + d.length)
+         for (j in d) {
+            d3.select(this).append("circle")
+            .attr("cx", j * fieldWidth / d.length
+               +Math.min(fieldWidth/d.length, fieldHeight / nodes.length)/2)
+            .attr("cy", i * fieldHeight / nodes.length
+               +Math.min(fieldWidth/d.length, fieldHeight / nodes.length)/2)
+            .attr("r", Math.min(fieldWidth / d.length, fieldHeight / nodes.length)/2)
+            .style("fill", function () { 
+               console.log(d[j])
+               if (d[j] >= 0) { return "blue"; }
+               else { return "red"; };
+            })
+            .style("opacity", 1/(1 + Math.exp(-Math.abs(d[j]))));
+         }
+      });
+   */
+};
+
 
